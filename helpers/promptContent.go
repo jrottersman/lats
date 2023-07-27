7907f58a3add18405ab09eae124790848541a5b0
Jake Rottersman <rottersmanjake@gmail.com>
2023-07-27 13:35:46 -0700

    refactor of prompt content to make it testable

--- helpers/promptContent.go
+++ helpers/promptContent.go
@@ -46,33 +46,6 @@ func PromptInput(p promptui.Prompt) string {
 }
 
 func PromptGetInput(pc PromptContent) string {
-	validate := func(input string) error {
-		if len(input) <= 0 {
-			return errors.New(pc.ErrorMsg)
-		}
-		return nil
-	}
-
-	templates := &promptui.PromptTemplates{
-		Prompt:  "{{ . }} ",
-		Valid:   "{{ . | green }} ",
-		Invalid: "{{ . | red }} ",
-		Success: "{{ . | bold }} ",
-	}
-
-	prompt := promptui.Prompt{
-		Label:     pc.Label,
-		Templates: templates,
-		Validate:  validate,
-	}
-
-	result, err := prompt.Run()
-	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("Input: %s\n", result)
-
-	return result
+	p := GeneratePrompt(pc)
+	return PromptInput(p)
 }
