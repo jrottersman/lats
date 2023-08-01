81d3a074a0b7e7decf2f45502aefefe2b521facb
Jake Rottersman <rottersmanjake@gmail.com>
2023-07-31 20:33:13 -0700

    create_rds_snapshot we are describing the snapshot

--- cmd/create_rds_snapshot.go
+++ cmd/create_rds_snapshot.go
@@ -1,19 +1,26 @@
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/jrottersman/lats/aws"
 	"github.com/spf13/cobra"
 )
 
 var (
+	// Variables used for flags
+	dbName string
+
 	CreateRDSSnapshotCmd = &cobra.Command{
 		Use:     "CreateRDSSnapshot",
 		Aliases: []string{"CreateSnapshot"},
 		Short:   "Creates a snapshot for a given DB",
 		Long:    "Creates a snapshot for an RDS or Aurora database",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("Copies a snapshot to a new region")
+			dbi := aws.Init("us-east-1") // TODO read this from config
+			dbi.GetInstance(dbName)
 		},
 	}
 )
+
+func init() {
+	CreateRDSSnapshotCmd.Flags().StringVarP(&dbName, "database-name", "d", "", "Database name we want to create the snapshot for")
+}
