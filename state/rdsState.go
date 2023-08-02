41481b45b696b35f9f2ff90139ceb9211bcdb095
Jake Rottersman <rottersmanjake@gmail.com>
2023-08-02 16:30:23 -0700

    first version of reader done

--- state/rdsState.go
+++ state/rdsState.go
@@ -8,13 +8,25 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/rds/types"
 )
 
+// EncodeRDSDatabaseOutput converts a dbInstace to an array of bytes in preperation for wrtiing it to disk
 func EncodeRDSDatabaseOutput(db *types.DBInstance) bytes.Buffer {
 	var encoder bytes.Buffer
 	enc := gob.NewEncoder(&encoder)
 
 	err := enc.Encode(db)
 	if err != nil {
-		log.Printf("Error encoding our database: %s", err)
+		log.fatalf("Error encoding our database: %s", err)
 	}
 	return encoder
+}
+
+// DecodeRDSDatabaseOutput takes a bytes buffer and returns it to a DbInstance type in preperation of restoring the database
+func DecodeRDSDatabaseOutput(b bytes.Buffer) types.DbInstance {
+	var dbInstance types.DBInstance
+	dec := gob.NewDecoder(&b)
+	err := dec.Decode(&dbInstance)
+	if err != nil {
+		log.Fatalf("Error decoding state for RDS Instance: %s", err)
+	}
+	return dbInstance
 }
