d40d65493a2069789a1981dfa7ce2c00b371f89b
Jake Rottersman <rottersmanjake@gmail.com>
2023-08-01 20:55:58 -0700

    commented the functions in rdsOperations

--- aws/rdsOperations.go
+++ aws/rdsOperations.go
@@ -10,16 +10,19 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/rds/types"
 )
 
+// Client is used for mocking the AWS RDS instance for testing
 type Client interface {
 	DescribeDBInstances(ctx context.Context, input *rds.DescribeDBInstancesInput, optFns ...func(*rds.Options)) (*rds.DescribeDBInstancesOutput, error)
 	CreateDBSnapshot(ctx context.Context, params *rds.CreateDBSnapshotInput, optFns ...func(*rds.Options)) (*rds.CreateDBSnapshotOutput, error)
 }
 
+//DbInstances holds our RDS client that allows for operations in AWS
 type DbInstances struct {
 	RdsClient Client
 }
 
 
+//GetInstance describes an RDS instance and returns it's output
 func (instances *DbInstances) GetInstance(instanceName string) (
 	*types.DBInstance, error) {
 	output, err := instances.RdsClient.DescribeDBInstances(context.TODO(),
@@ -40,6 +43,9 @@ func (instances *DbInstances) GetInstance(instanceName string) (
 	}
 }
 
+// CreateSnapshot cretaes an AWS snapshot
+// :instanceName - name of the database we want to backup
+// :snapShotName name of the backup we are creating
 func (instances *DbInstances) CreateSnapshot(instanceName string, snapshotName string) (
 	*types.DBSnapshot, error) {
 	output, err := instances.RdsClient.CreateDBSnapshot(context.TODO(), &rds.CreateDBSnapshotInput{
