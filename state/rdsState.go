77ae331ce82e2c070518de9f616e6ebbef369f3b
Jake Rottersman <rottersmanjake@gmail.com>
2023-08-05 11:34:26 -0700

    dbSnapshot encoding and decoding

--- state/rdsState.go
+++ state/rdsState.go
@@ -30,3 +30,27 @@ func DecodeRDSDatabaseOutput(b bytes.Buffer) types.DBInstance {
 	}
 	return dbInstance
 }
+
+// EncodeRDSSnapshotOutput converts a DbSnapshot struct to an array of bytes in preperation for wrtiing it to disk
+func EncodeRDSSnapshotOutput(snapshot types.DBSnapshot) bytes.Buffer {
+	var encoder bytes.Buffer
+	enc := gob.NewEncoder(&encoder)
+
+	err := enc.Encode(snapshot)
+	if err != nil {
+		log.Fatalf("Error encoding our snapshot: %s", err)
+	}
+	return encoder
+}
+
+
+// DecodeRDSDatabaseOutput takes a bytes buffer and returns it to a DbInstance type in preperation of restoring the database
+func DecodeRDSDatabaseOutput(b bytes.Buffer) types.DBSnapshot {
+	var dbSnapshot types.DBSnapshot
+	dec := gob.NewDecoder(&b)
+	err := dec.Decode(&dbSnapshot)
+	if err != nil {
+		log.Fatalf("Error decoding state for snapshot: %s", err)
+	}
+	return dbSnapshot
+}
