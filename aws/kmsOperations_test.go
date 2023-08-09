bfa539e104b057ccb7ab49554b174caab6e07986
Jake Rottersman <rottersmanjake@gmail.com>
2023-08-08 20:48:39 -0700

    got kms test working

---
+++ aws/kmsOperations_test.go
@@ -0,0 +1,36 @@
+package aws
+
+import (
+	"context"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/kms"
+	"github.com/aws/aws-sdk-go-v2/service/kms/types"
+)
+
+type mockKMSClient struct{}
+
+func (m mockKMSClient) CreateKey(ctx context.Context, params *kms.CreateKeyInput, optFns ...func(*kms.Options)) (*kms.CreateKeyOutput, error) {
+	kid := "foobar"
+	r := kms.CreateKeyOutput{
+		KeyMetadata: &types.KeyMetadata{
+			KeyId: aws.String(kid),
+		},
+	}
+	return &r, nil
+}
+
+func TestCreateKMSKey(t *testing.T) {
+	c := mockKMSClient{}
+	kmsOp := KmsOperations{
+		Client: c,
+	}
+	results, err := kmsOp.CreateKMSKey()
+	if err != nil {
+		t.Errorf("error calling CreateKMSKey %s", err)
+	}
+	if *results.KeyId != "foobar" {
+		t.Errorf("got %s expected foobar", *results.KeyId)
+	}
+}
