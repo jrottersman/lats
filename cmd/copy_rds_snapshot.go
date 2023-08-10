135af8b00285869fd7f9cf7bd556974f986c3a38
Jake Rottersman <rottersmanjake@gmail.com>
2023-08-10 14:28:42 -0700

    initial work on create snapshot

--- cmd/copy_rds_snapshot.go
+++ cmd/copy_rds_snapshot.go
@@ -13,7 +13,11 @@ var (
 		Short:   "Copies a snapshot for a given DB",
 		Long:    "Copies a snapshot for an RDS or Aurora database into a new region",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("TODO implement me")
+			createSnapshot()
 		},
 	}
 )
+
+func createSnapshot() {
+	fmt.Println("TODO implement me")
+}
