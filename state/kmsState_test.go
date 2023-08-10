bc345cf7802bb699c53a55ab8e0c7cbbfe1cebc3
Jake Rottersman <rottersmanjake@gmail.com>
2023-08-09 19:55:24 -0700

    kmsState test

---
+++ state/kmsState_test.go
@@ -0,0 +1,25 @@
+package state
+
+import (
+	"encoding/gob"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/kms/types"
+)
+
+func TestEncodeKmsOutput(t *testing.T) {
+	kmd := types.KeyMetadata{
+		KeyId: aws.String("foo"),
+	}
+	b := EncodeKmsOutput(&kmd)
+	var result types.KeyMetadata
+	dec := gob.NewDecoder(&b)
+	err := dec.Decode(&result)
+	if err != nil {
+		t.Errorf("decode error: %s", err)
+	}
+	if *result.KeyId != *kmd.KeyId {
+		t.Errorf("got %s expected %s", *result.KeyId, *kmd.KeyId)
+	}
+}
