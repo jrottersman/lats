8d335a803cae635650c2ab8cee231d293eab1eac
Jake Rottersman <rottersmanjake@gmail.com>
2023-08-19 10:22:48 -0700

    getClusterId helper

---
+++ helpers/getters_test.go
@@ -0,0 +1,25 @@
+package helpers
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/rds/types"
+)
+
+func TestGetClusterId(t *testing.T) {
+	ni := types.DBInstance{}
+	nr := GetClusterId(ni)
+	if nr != nil {
+		t.Errorf("this should be nil")
+	}
+
+	i := types.DBInstance{
+		DBClusterIdentifier: aws.String("foobar"),
+	}
+	r := GetClusterId(i)
+	if *r != "foobar" {
+		t.Errorf("got %s expected foobar", *r)
+	}
+
+}
