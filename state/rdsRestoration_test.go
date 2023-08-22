0b744fcac48c4acd36743d90c90cb252d07a0c66
Jake Rottersman <rottersmanjake@gmail.com>
2023-08-21 20:25:43 -0700

    get snapshot identifier test added

--- state/rdsRestoration_test.go
+++ state/rdsRestoration_test.go
@@ -175,3 +175,39 @@ func TestRDSRestorationStore_GetInstanceClass(t *testing.T) {
 		})
 	}
 }
+
+func TestRDSRestorationStore_GetSnapshotIdentifier(t *testing.T) {
+	type fields struct {
+		Snapshot *types.DBSnapshot
+		Instance *types.DBInstance
+		Cluster  *types.DBCluster
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *string
+	}{
+		{name: "totalNil", fields: fields{nil, nil, nil}, want: nil},
+		{name: "RegularNil", fields: fields{&types.DBSnapshot{}, nil, nil}, want: nil},
+		{name: "Value", fields: fields{&types.DBSnapshot{DBSnapshotIdentifier: aws.String("foo")}, nil, nil}, want: aws.String("foo")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := RDSRestorationStore{
+				Snapshot: tt.fields.Snapshot,
+				Instance: tt.fields.Instance,
+				Cluster:  tt.fields.Cluster,
+			}
+			got := r.GetSnapshotIdentifier()
+			if tt.want == nil {
+				if got != tt.want {
+					t.Errorf("RDSRestorationStore.GetSnapshotIdentifier() = %v, want %v", got, tt.want)
+				}
+			} else {
+				if *got != *tt.want {
+					t.Errorf("RDSRestorationStore.GetSnapshotIdentifier() = %v, want %v", *got, *tt.want)
+				}
+			}
+		})
+	}
+}
