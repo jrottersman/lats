793537147f9cfa1d9840366c1967369617952b32
Jake Rottersman <rottersmanjake@gmail.com>
2023-08-25 19:31:09 -0700

    get engine for cluster snapshot test

--- state/rdsRestoration_test.go
+++ state/rdsRestoration_test.go
@@ -287,3 +287,41 @@ func TestRDSRestorationStore_GetDBClusterIdentifier(t *testing.T) {
 		})
 	}
 }
+
+func TestRDSRestorationStore_GetClusterEngine(t *testing.T) {
+	type fields struct {
+		Snapshot        *types.DBSnapshot
+		Instance        *types.DBInstance
+		Cluster         *types.DBCluster
+		ClusterSnapshot *types.DBClusterSnapshot
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *string
+	}{
+		{name: "totalNil", fields: fields{nil, nil, nil, nil}, want: nil},
+		{name: "RegularNil", fields: fields{nil, nil, nil, &types.DBClusterSnapshot{}}, want: nil},
+		{name: "Value", fields: fields{nil, nil, nil, &types.DBClusterSnapshot{Engine: aws.String("foo")}}, want: aws.String("foo")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := RDSRestorationStore{
+				Snapshot:        tt.fields.Snapshot,
+				Instance:        tt.fields.Instance,
+				Cluster:         tt.fields.Cluster,
+				ClusterSnapshot: tt.fields.ClusterSnapshot,
+			}
+			got := r.GetClusterEngine()
+			if tt.want == nil {
+				if got != tt.want {
+					t.Errorf("RDSRestorationStore.GetClusterEngine() = %v, want %v", got, tt.want)
+				}
+			} else {
+				if *got != *tt.want {
+					t.Errorf("RDSRestorationStore.GetClusterEngine() = %v, want %v", got, tt.want)
+				}
+			}
+		})
+	}
+}
