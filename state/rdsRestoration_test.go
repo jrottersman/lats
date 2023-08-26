48e7b3d9c79e461b8ca45594088517e942f4002f
Jake Rottersman <rottersmanjake@gmail.com>
2023-08-25 19:36:36 -0700

    GetKMSKey test

--- state/rdsRestoration_test.go
+++ state/rdsRestoration_test.go
@@ -325,3 +325,41 @@ func TestRDSRestorationStore_GetClusterEngine(t *testing.T) {
 		})
 	}
 }
+
+func TestRDSRestorationStore_GetKmsKey(t *testing.T) {
+	type fields struct {
+		Snapshot        *types.DBSnapshot
+		Instance        *types.DBInstance
+		Cluster         *types.DBCluster
+		ClusterSnapshot *types.DBClusterSnapshot
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *string
+	}{
+		{name: "totalNil", fields: fields{nil, nil, nil, nil}, want: nil},
+		{name: "RegularNil", fields: fields{nil, nil, nil, &types.DBClusterSnapshot{}}, want: nil},
+		{name: "Value", fields: fields{nil, nil, nil, &types.DBClusterSnapshot{KmsKeyId: aws.String("foo")}}, want: aws.String("foo")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := RDSRestorationStore{
+				Snapshot:        tt.fields.Snapshot,
+				Instance:        tt.fields.Instance,
+				Cluster:         tt.fields.Cluster,
+				ClusterSnapshot: tt.fields.ClusterSnapshot,
+			}
+			got := r.GetKmsKey()
+			if tt.want == nil {
+				if got != tt.want {
+					t.Errorf("RDSRestorationStore.GetKmsKey() = %v, want %v", got, tt.want)
+				}
+			} else {
+				if *got != *tt.want {
+					t.Errorf("RDSRestorationStore.GetKmsKey() = %v, want %v", got, tt.want)
+				}
+			}
+		})
+	}
+}
