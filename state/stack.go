181ecc39289f2b37eb97b2808cffae197ad60a5d
Jake Rottersman <rottersmanjake@gmail.com>
2023-08-27 13:38:36 -0700

    Create NewStack in progress

--- state/stack.go
+++ state/stack.go
@@ -19,3 +19,20 @@ type Stack struct {
 	RestorationObjectName string
 	Objects               map[int][]Object //int is the order in which we restore
 }
+
+func NewStack(name string, restorationObjectName string, objects []Object) Stack {
+	objs := make(map[int][]Object)
+	for _, v := range objects {
+		order := v.Order
+		_, ok := objs[order]
+		if !ok {
+			objs[order] = []Object{v}
+		}
+	}
+
+	return Stack{
+		Name:                  name,
+		RestorationObjectName: restorationObjectName,
+		Objects:               objs,
+	}
+}
