628a0041e0c16079fc80d3f987dcaa7121f99cf3
Jake Rottersman <rottersmanjake@gmail.com>
2023-08-27 14:45:38 -0700

    test creating objects store

--- state/stack_test.go
+++ state/stack_test.go
@@ -16,3 +16,26 @@ func Test_NewObject(t *testing.T) {
 		t.Errorf("NewObject order expected %d got %d", resp.Order, order)
 	}
 }
+
+func Test_NewStack(t *testing.T) {
+	name := "foo"
+	roname := "bar"
+
+	o1 := Object{
+		rds.RestoreDBClusterFromSnapshotInput{},
+		1,
+		"RDSCluster",
+	}
+
+	o2 := Object{
+		rds.RestoreDBClusterFromS3Input{},
+		1,
+		"RDSCluster",
+	}
+	objects := []Object{o1, o2}
+
+	resp := NewStack(name, roname, objects)
+	if len(resp.Objects[1]) != 2 {
+		t.Errorf("expected 2 got %d", len(resp.Objects[1]))
+	}
+}
