faeafa3a886657c9651eee69a79023bd5d3a7ca0
Jake Rottersman <rottersmanjake@gmail.com>
2023-08-28 19:21:14 -0700

    test stack encoder

--- state/stack_test.go
+++ state/stack_test.go
@@ -1,6 +1,7 @@
 package state
 
 import (
+	"encoding/gob"
 	"testing"
 )
 
@@ -35,5 +36,39 @@ func Test_NewStack(t *testing.T) {
 	resp := NewStack(name, roname, objects)
 	if len(resp.Objects[1]) != 2 {
 		t.Errorf("expected 2 got %d", len(resp.Objects[1]))
+	}
+}
+
+func Test_Encoder(t *testing.T) {
+	name := "foo"
+	roname := "bar"
+
+	o1 := Object{
+		"tmp/foo",
+		1,
+		"RDSCluster",
+	}
+
+	o2 := Object{
+		"tmp/bar",
+		1,
+		"RDSCluster",
+	}
+	objects := []Object{o1, o2}
+
+	stack := NewStack(name, roname, objects)
+
+	r, err := stack.Encoder()
+	if err != nil {
+		t.Errorf("encode error: %s", err)
+	}
+	var result Stack
+	dec := gob.NewDecoder(r)
+	err = dec.Decode(&result)
+	if err != nil {
+		t.Errorf("decode error: %s", err)
+	}
+	if result.Name != stack.Name {
+		t.Errorf("got %s expected %s", result.Name, stack.Name)
 	}
 }
