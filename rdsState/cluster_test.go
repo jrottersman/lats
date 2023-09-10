44af0891bc7bc5bb2604f2a62ef378b2b5075625
Jake Rottersman <rottersmanjake@gmail.com>
2023-09-10 11:05:42 -0700

    test failing but progress

--- rdsState/cluster_test.go
+++ rdsState/cluster_test.go
@@ -1,10 +1,12 @@
-package rdsState
+package rdsState_test
 
 import (
 	"reflect"
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/service/rds/types"
 	"github.com/jrottersman/lats/aws"
+	"github.com/jrottersman/lats/rdsState"
 	"github.com/jrottersman/lats/state"
 )
 
@@ -16,17 +18,36 @@ func TestGenerateRDSClusterStack(t *testing.T) {
 		client aws.DbInstances
 		folder string
 	}
+
+	clusterId := "foo"
+	snapshotId := "bar"
+	filen := "/tmp/bar"
+	c := mockRDSClient{}
+	i := aws.DbInstances{c}
+
+	resto := state.RDSRestorationStore{
+		Cluster:         &types.DBCluster{DBClusterIdentifier: &clusterId},
+		ClusterSnapshot: &types.DBClusterSnapshot{DBClusterSnapshotIdentifier: &snapshotId},
+	}
+	arg := args{
+		r:      resto,
+		name:   "foo",
+		fn:     &filen,
+		client: i,
+		folder: "/tmp",
+	}
+
 	tests := []struct {
 		name    string
 		args    args
 		want    *state.Stack
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{"test", arg, nil, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := GenerateRDSClusterStack(tt.args.r, tt.args.name, tt.args.fn, tt.args.client, tt.args.folder)
+			got, err := rdsState.GenerateRDSClusterStack(tt.args.r, tt.args.name, tt.args.fn, tt.args.client, tt.args.folder)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GenerateRDSClusterStack() error = %v, wantErr %v", err, tt.wantErr)
 				return
