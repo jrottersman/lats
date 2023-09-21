da3df1c012fa836108d6b6915d28e16f60bccdf6
Jake Rottersman <rottersmanjake@gmail.com>
2023-09-20 20:22:45 -0700

    restore snapshot progress

--- cmd/restore_rds_snapshot.go
+++ cmd/restore_rds_snapshot.go
@@ -12,6 +12,7 @@ var (
 	//Variables used for flags
 	restoreSnapshotName string
 	restoreDbName       string
+	region              string
 
 	//RestoreRDSSnapshotCmd restores an RDS snapshot
 	RestoreRDSSnapshotCmd = &cobra.Command{
@@ -28,15 +29,19 @@ var (
 func init() {
 	RestoreRDSSnapshotCmd.Flags().StringVarP(&restoreSnapshotName, "snapshot-name", "s", "", "name of the snapshot we want to restore: choose one of snapshotName or db name")
 	RestoreRDSSnapshotCmd.Flags().StringVarP(&restoreDbName, "database-name", "d", "", "name of the database we want to restore the snapshot for")
+	RestoreRDSSnapshotCmd.Flags().StringVarP(&region, "region", "r", "", "AWS region we are restoring in")
 }
 
 //RestoreSnapshot is the function that restores a snapshot
 func RestoreSnapshot(instances *aws.DbInstances, stateKV state.StateManager, snapshotName string) error {
+	dbi := aws.Init(region)
 	SnapshotStack := FindStack(stateKV, snapshotName)
 	if SnapshotStack.RestorationObjectName == state.Cluster {
-		fmt.Printf("restoring a cluster")
+		return dbi.CreateClusterFromStack(SnapshotStack)
+		// TODO finish create Cluster from stack
 	} else if SnapshotStack.RestorationObjectName == state.LoneInstance {
 		fmt.Printf("restoring an instance")
+		//TODO create CreateInstanceFromStack
 	}
-	return nil
+	return fmt.Errorf("Error invalid type of stack to restore a snapshot")
 }
