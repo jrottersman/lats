c26f910d190d13f30853c4163846ee30c6f75a10
Jake Rottersman <rottersmanjake@gmail.com>
2023-10-21 20:26:58 -0700

    Removed unused function

--- helpers/fileNameGen.go
+++ helpers/fileNameGen.go
@@ -2,7 +2,6 @@ package helpers
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/google/uuid"
 )
@@ -11,9 +10,4 @@ func RandomStateFileName() *string {
 	u := uuid.New()
 	filename := fmt.Sprintf(".state/%s.gob", u)
 	return &filename
-}
-
-func SnapshotName(db string) string {
-	t := time.Now().UTC().String()
-	return fmt.Sprintf("%s-%s", db, t)
 }
