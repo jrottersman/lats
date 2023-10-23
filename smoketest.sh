36c22c28c8a80dcc34243fb65a2d2f817ba88fc6
Jake Rottersman <rottersmanjake@gmail.com>
2023-10-22 19:01:05 -0700

    restore smoke test

--- smoketest.sh
+++ smoketest.sh
@@ -2,3 +2,4 @@ go build .
 ./lats init
 ./lats CreateRDSSnapshot --database-name test --snapshot-name test
 ./lats CopyRDSSnapshot --snapshot test --new-snapshot test2
+/lats restoreRDSSnapshot --snapshot-name test2 --database-name testRestore --region us-east-2
