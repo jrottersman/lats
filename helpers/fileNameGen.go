357a87b5578eef232cb19dacf620dab43bc687fa
Jake Rottersman <rottersmanjake@gmail.com>
2023-11-21 18:22:19 -0800

    Generate instance name

--- helpers/fileNameGen.go
+++ helpers/fileNameGen.go
@@ -11,3 +11,9 @@ func RandomStateFileName() *string {
 	filename := fmt.Sprintf("%s.gob", u)
 	return &filename
 }
+
+func InstanceName() *string {
+	u := uuid.New()
+	instanceName := fmt.Sprintf("instance-%s", u)
+	return &instanceName
+}
