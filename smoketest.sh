15b04ea5236e2d4f58054c6fb177bde24995e653
Jake Rottersman <rottersmanjake@gmail.com>
2023-11-24 13:09:27 -0800

    Starting to fix smoke test

--- smoketest.sh
+++ smoketest.sh
@@ -2,6 +2,10 @@ go build .
 ./lats init
 ./lats CreateRDSSnapshot --database-name test --snapshot-name test
 echo "sleep for five minutes to create snapshot TODO change this to actually check status and then go to next step"
+while :
+do 
+    aws rds describe-db-snapshot --snapshot-identifier test
+done
 sleep 300
 ./lats CopyRDSSnapshot --snapshot test --new-snapshot test2
 echo "sleep for five minutes to copy snapshot TODO change this as above"
