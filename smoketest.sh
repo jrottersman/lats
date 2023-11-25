29597a2d25715c1127cf30333b21c70fe1d8e95c
Jake Rottersman <rottersmanjake@gmail.com>
2023-11-24 17:24:40 -0800

    more looping in smoke test

--- smoketest.sh
+++ smoketest.sh
@@ -13,6 +13,15 @@ do
     sleep 15
 done
 ./lats CopyRDSSnapshot --snapshot test --new-snapshot test2
-echo "sleep for five minutes to copy snapshot TODO change this as above"
-sleep 300
+while :
+do 
+    snapStatus=`aws rds describe-db-snapshots --db-snapshot-identifier test2 --output json | jq ".DBSnapshots[] | .Status"`
+    if [$snapStatus = "available"]
+    then    
+        echo "Snapshot complete"
+        break
+    fi
+    echo "snapshot in progress"
+    sleep 15
+done
 ./lats restoreRDSSnapshot --snapshot-name test2 --database-name testRestore --region us-east-2 --subnet-group sg-name
