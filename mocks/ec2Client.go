9824d392e80170a64540ed1c7ca5ab655a72a28a
Jake Rottersman <rottersmanjake@gmail.com>
2023-12-05 16:48:57 -0800

    mock complete

--- mocks/ec2Client.go
+++ mocks/ec2Client.go
@@ -1,4 +1,17 @@
 package mock
 
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+)
+
 type MockEC2Client struct {
+}
+
+func (m MockEC2Client) CreateSecurityGroup(ctx context.Context, params *ec2.CreateSecurityGroupInput) (*ec2.CreateSecurityGroupOutput, error) {
+	return &ec2.CreateSecurityGroupOutput{
+		GroupId: aws.String("foobar"),
+	}, nil
 }
