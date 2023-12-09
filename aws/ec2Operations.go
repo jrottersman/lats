3ae360d9b4d34c189bc3923b85632672977a8f69
Jake Rottersman <rottersmanjake@gmail.com>
2023-12-09 14:11:02 -0800

    Ec2 test and ingress

--- aws/ec2Operations.go
+++ aws/ec2Operations.go
@@ -12,6 +12,7 @@ type Ec2Client interface {
 	CreateSecurityGroup(ctx context.Context, params *ec2.CreateSecurityGroupInput) (*ec2.CreateSecurityGroupOutput, error)
 	AuthorizeSecurityGroupEgress(ctx context.Context, params *ec2.AuthorizeSecurityGroupEgressInput) (*ec2.AuthorizeSecurityGroupEgressOutput, error)
 	DescribeSecurityGroups(ctx context.Context, params *ec2.DescribeSecurityGroupsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeSecurityGroupsOutput, error)
+	AuthorizeSecurityGroupIngress(ctx context.Context, params *ec2.AuthorizeSecurityGroupIngressInput, optFns ...func(*ec2.Options)) (*ec2.AuthorizeSecurityGroupIngressOutput, error)
 }
 
 type SGEgressInput struct {
--- aws/ec2Operations_test.go
+++ aws/ec2Operations_test.go
@@ -96,3 +96,36 @@ func TestEC2Instances_SGEgress(t *testing.T) {
 		})
 	}
 }
+
+func TestEC2Instances_DescribeSG(t *testing.T) {
+	type fields struct {
+		Client Ec2Client
+	}
+	type args struct {
+		sgName string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *ec2.DescribeSecurityGroupsOutput
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &EC2Instances{
+				Client: tt.fields.Client,
+			}
+			got, err := c.DescribeSG(tt.args.sgName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("EC2Instances.DescribeSG() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("EC2Instances.DescribeSG() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
