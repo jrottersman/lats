b18cff76e0cb0bbda0b9629ddab34de0b6845376
Jake Rottersman <rottersmanjake@gmail.com>
2023-12-09 16:49:28 -0800

    added tesT

--- aws/ec2Operations_test.go
+++ aws/ec2Operations_test.go
@@ -129,3 +129,44 @@ func TestEC2Instances_DescribeSG(t *testing.T) {
 		})
 	}
 }
+
+func TestEC2Instances_SGIngress(t *testing.T) {
+	tr := true
+	type fields struct {
+		Client Ec2Client
+	}
+	type args struct {
+		s SGInput
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *ec2.AuthorizeSecurityGroupIngressOutput
+		wantErr bool
+	}{
+		{name: "test",
+			fields: fields{Client: mock.MockEC2Client{}},
+			args:   args{s: SGInput{}},
+			want: &ec2.AuthorizeSecurityGroupIngressOutput{
+				Return: &tr,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &EC2Instances{
+				Client: tt.fields.Client,
+			}
+			got, err := c.SGIngress(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("EC2Instances.SGIngress() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("EC2Instances.SGIngress() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
