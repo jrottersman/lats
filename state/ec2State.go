c98e8fde144aea6d57bbdbe26d6b3f78fc7bbc53
Jake Rottersman <rottersmanjake@gmail.com>
2023-12-19 18:26:03 -0800

    encode state

--- state/ec2State.go
+++ state/ec2State.go
@@ -1,1 +1,20 @@
 package state
+
+import (
+	"bytes"
+	"encoding/gob"
+	"log/slog"
+
+	"github.com/jrottersman/lats/cmd"
+)
+
+func EncodeSecurityGroups(sg cmd.SecurityGroupOutput) bytes.Buffer {
+	var encoder bytes.Buffer
+	enc := gob.NewEncoder(&encoder)
+
+	err := enc.Encode(sg)
+	if err != nil {
+		slog.Error("Error encoding our database", "error", err)
+	}
+	return encoder
+}
