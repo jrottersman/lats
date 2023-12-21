42c54d0c8ef94d4d522c718fb47b04a5e0488839
Jake Rottersman <rottersmanjake@gmail.com>
2023-12-20 17:55:28 -0800

    added test

--- state/ec2State_test.go
+++ state/ec2State_test.go
@@ -27,3 +27,18 @@ func TestEncodeSecurityGroups(t *testing.T) {
 		t.Errorf("got %d expected %d", len(result.SecurityGroups), len(sg2.SecurityGroups))
 	}
 }
+
+func TestDecodeSecurityGroups(t *testing.T) {
+
+	sg := types.SecurityGroup{
+		Description: aws.String("foo"),
+	}
+	sg2 := SecurityGroupOutput{
+		SecurityGroups: []types.SecurityGroup{sg},
+	}
+	r := EncodeSecurityGroups(sg2)
+	result := DecodeSecurityGroups(r)
+	if len(result.SecurityGroups) != len(sg2.SecurityGroups) {
+		t.Errorf("got %d expected %d", len(result.SecurityGroups), len(sg2.SecurityGroups))
+	}
+}
