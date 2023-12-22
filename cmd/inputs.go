372e0d3ec19346d2d96c4a1ea1da4810dc9ca9f0
Jake Rottersman <rottersmanjake@gmail.com>
2023-12-21 19:44:33 -0800

    make it a pointer

--- cmd/inputs.go
+++ cmd/inputs.go
@@ -13,7 +13,7 @@ type CreateClusterSnapshotInput struct {
 	sm      state.StateManager
 	cluster *types.DBCluster
 	sfn     string
-	sgs     state.SecurityGroupOutput
+	sgs     *state.SecurityGroupOutput
 }
 
 //CreateInstanceSnapshotInput input for create snapshot for instance
@@ -22,5 +22,5 @@ type CreateInstanceSnapshotInput struct {
 	ec2 aws.EC2Instances
 	sm  state.StateManager
 	sfn string
-	sgs state.SecurityGroupOutput
+	sgs *state.SecurityGroupOutput
 }
