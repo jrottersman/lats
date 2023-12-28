89c3e21f25e92345f637e6b6ea63b75ab9b7ffa5
Jake Rottersman <rottersmanjake@gmail.com>
2023-12-28 13:27:56 -0800

    instance sg

--- aws/rdsOperations.go
+++ aws/rdsOperations.go
@@ -324,7 +324,14 @@ func (instances *DbInstances) CreateInstanceFromStack(c CreateInstanceFromStackI
 			pb := p.ReadObject()
 			switch pb.(type) {
 			case *state.SecurityGroupOutput:
-				slog.Info("TODO handle security groups")
+				sgs, ok := pb.(*state.SecurityGroupOutput)
+				if !ok {
+					slog.Error("error casting security group")
+					return fmt.Errorf("Type error security group")
+				}
+				for _, v := range sgs.SecurityGroups {
+					createSecurityGroup(v)
+				}
 			case *pgstate.ParameterGroup:
 				pg, ok := pb.(*pgstate.ParameterGroup)
 				if !ok {
