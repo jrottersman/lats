b16dfe41d19eb2c31faea7141db6e46e5ad6f16d
Jake Rottersman <rottersmanjake@gmail.com>
2023-12-30 14:36:40 -0800

    create vpc

--- aws/rdsOperations.go
+++ aws/rdsOperations.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/aws/aws-sdk-go-v2/service/rds"
 	"github.com/aws/aws-sdk-go-v2/service/rds/types"
 	"github.com/jrottersman/lats/pgstate"
@@ -135,10 +134,6 @@ func (instances *DbInstances) CreateDBSubnetGroup(name string, description strin
 		SubnetIds:                subnets,
 	})
 	return group, err
-}
-
-func createSecurityGroup(sg ec2types.SecurityGroup) {
-	fmt.Printf("implement me")
 }
 
 //CreateClusterFromStackInput creates a stack all inputs required
