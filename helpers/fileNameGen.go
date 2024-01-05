2d78910a861955a9fce19a997800b5ed593b886f
Jake Rottersman <rottersmanjake@gmail.com>
2024-01-04 17:44:05 -0800

    more lint

--- helpers/fileNameGen.go
+++ helpers/fileNameGen.go
@@ -6,12 +6,14 @@ import (
 	"github.com/google/uuid"
 )
 
+// RandomStateFileName generates a gob file name starting with a UUID
 func RandomStateFileName() *string {
 	u := uuid.New()
 	filename := fmt.Sprintf("%s.gob", u)
 	return &filename
 }
 
+// InstanceName generates a new random instance name for when one isn't provided
 func InstanceName() *string {
 	u := uuid.New()
 	instanceName := fmt.Sprintf("instance-%s", u)
