da285d44ac97b8c669b28c134bcb1c95bca4a7a6
Jake Rottersman <rottersmanjake@gmail.com>
2024-01-12 13:41:36 -0800

    more lint

--- helpers/getters.go
+++ helpers/getters.go
@@ -2,7 +2,7 @@ package helpers
 
 import "github.com/aws/aws-sdk-go-v2/service/rds/types"
 
-// GetClusterId get's the clsuter id or nil as a getter this is the only one in helpers because it's needed for the initial load
+// GetClusterID get's the clsuter id or nil as a getter this is the only one in helpers because it's needed for the initial load
 func GetClusterID(t *types.DBInstance) *string {
 	if t.DBClusterIdentifier == nil {
 		return nil
