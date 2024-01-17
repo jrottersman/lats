18afb0941a7969fc71b6390f02acadc894969050
Jake Rottersman <rottersmanjake@gmail.com>
2024-01-17 15:08:05 -0800

    mock update

--- mocks/ec2Client.go
+++ mocks/ec2Client.go
@@ -2,34 +2,38 @@ package mock
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 )
 
-//EC2Client is a client for ec2 that's a mock
+// EC2Client is a client for ec2 that's a mock
 type EC2Client struct {
 }
 
-//CreateSecurityGroup mock security group creaton
+// CreateSecurityGroup mock security group creaton
 func (m EC2Client) CreateSecurityGroup(ctx context.Context, params *ec2.CreateSecurityGroupInput, optFns ...func(*ec2.Options)) (*ec2.CreateSecurityGroupOutput, error) {
+	if params.Description == nil {
+		return nil, fmt.Errorf("nil description error this is fake")
+	}
 	return &ec2.CreateSecurityGroupOutput{
 		GroupId: aws.String("foobar"),
 	}, nil
 }
 
-//AuthorizeSecurityGroupEgress mock authorize security group egress
+// AuthorizeSecurityGroupEgress mock authorize security group egress
 func (m EC2Client) AuthorizeSecurityGroupEgress(ctx context.Context, params *ec2.AuthorizeSecurityGroupEgressInput, optFns ...func(*ec2.Options)) (*ec2.AuthorizeSecurityGroupEgressOutput, error) {
 	boo := true
 	return &ec2.AuthorizeSecurityGroupEgressOutput{Return: &boo}, nil
 }
 
-//DescribeSecurityGroups mock descirbe security groups
+// DescribeSecurityGroups mock descirbe security groups
 func (m EC2Client) DescribeSecurityGroups(ctx context.Context, params *ec2.DescribeSecurityGroupsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeSecurityGroupsOutput, error) {
 	return &ec2.DescribeSecurityGroupsOutput{}, nil
 }
 
-//AuthorizeSecurityGroupIngress another mock
+// AuthorizeSecurityGroupIngress another mock
 func (m EC2Client) AuthorizeSecurityGroupIngress(ctx context.Context, params *ec2.AuthorizeSecurityGroupIngressInput, optFns ...func(*ec2.Options)) (*ec2.AuthorizeSecurityGroupIngressOutput, error) {
 	boo := true
 	return &ec2.AuthorizeSecurityGroupIngressOutput{Return: &boo}, nil
