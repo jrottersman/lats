49f60e539c45bbeb7d04bbda7ca0ec45afe6c398
Jake Rottersman <rottersmanjake@gmail.com>
2024-02-09 13:21:21 -0800

    allow us to unmarshall the whole thing

--- cmd/restore_rds_snapshot_types.go
+++ cmd/restore_rds_snapshot_types.go
@@ -2,7 +2,16 @@ package cmd
 
 type sgRulesStruct struct {
 	ruleType string `yaml:"type"`
-	protocol string
-	port     int
-	source   string
+	protocol string `yaml:"protocol"`
+	port     int    `yaml:"port"`
+	source   string `yaml:"source"`
+}
+
+type sgStruct struct {
+	snapshot          string
+	database          string
+	region            string
+	dbsubnetgroupname string
+	subnets           []string
+	securitygroups    []sgRulesStruct
 }
