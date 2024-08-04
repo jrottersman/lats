18ba015b4296b4c0aef7d33ebe8b127465cc1f7b
Jake Rottersman <rottersmanjake@gmail.com>
2024-08-03 17:04:44 -0700

    describe vpc

--- aws/ec2Operations.go
+++ aws/ec2Operations.go
@@ -17,6 +17,7 @@ type Ec2Client interface {
 	DescribeSecurityGroups(ctx context.Context, params *ec2.DescribeSecurityGroupsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeSecurityGroupsOutput, error)
 	AuthorizeSecurityGroupIngress(ctx context.Context, params *ec2.AuthorizeSecurityGroupIngressInput, optFns ...func(*ec2.Options)) (*ec2.AuthorizeSecurityGroupIngressOutput, error)
 	DescribeSubnets(ctx context.Context, params *ec2.DescribeSubnetsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeSubnetsOutput, error)
+	DescribeVpcs(ctx context.Context, params *ec2.DescribeVpcsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeVpcsOutput, error)
 }
 
 // CreateSGInput input for the create SG function
