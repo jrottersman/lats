c7074b8e9a0851f0640a60b5970ff5231d23169e
Jake Rottersman <rottersmanjake@gmail.com>
2025-01-04 08:00:19 -0800

    Azs

--- aws/ec2Operations.go
+++ aws/ec2Operations.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
-	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/jrottersman/lats/state"
 )
 
@@ -21,7 +20,7 @@ type Ec2Client interface {
 	DescribeVpcs(ctx context.Context, params *ec2.DescribeVpcsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeVpcsOutput, error)
 	DescribeInternetGateways(ctx context.Context, params *ec2.DescribeInternetGatewaysInput, optFns ...func(*ec2.Options)) (*ec2.DescribeInternetGatewaysOutput, error)
 	DescribeRouteTables(ctx context.Context, params *ec2.DescribeRouteTablesInput, optFns ...func(*ec2.Options)) (*ec2.DescribeRouteTablesOutput, error)
-	DescribeAvailabilityZonesRequest(input *ec2.DescribeAvailabilityZonesInput) (req *request.Request, output *ec2.DescribeAvailabilityZonesOutput)
+	DescribeAvailabilityZones(ctx context.Context, params *ec2.DescribeAvailabilityZonesInput, optFns ...func(*ec2.Options)) (*ec2.DescribeAvailabilityZonesOutput, error)
 }
 
 // CreateSGInput input for the create SG function
