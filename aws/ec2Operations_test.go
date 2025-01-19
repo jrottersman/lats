0f17a8d303280af30fd251a251e919114eec2788
Jake Rottersman <rottersmanjake@gmail.com>
2025-01-19 09:51:10 -0800

    failing test

--- aws/ec2Operations_test.go
+++ aws/ec2Operations_test.go
@@ -262,39 +262,6 @@ func TestEC2Instances_SGEgress(t *testing.T) {
 	}
 }
 
-func TestEC2Instances_GetSubnets(t *testing.T) {
-	type fields struct {
-		Client Ec2Client
-	}
-	type args struct {
-		sgIds []string
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *ec2.DescribeSubnetsOutput
-		wantErr bool
-	}{
-		{name: "test", fields: fields{Client: mock.EC2Client{}}, args: args{sgIds: []string{"foo"}}, want: &ec2.DescribeSubnetsOutput{}, wantErr: false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &EC2Instances{
-				Client: tt.fields.Client,
-			}
-			got, err := c.GetSubnets(tt.args.sgIds)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("EC2Instances.GetSGs() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("EC2Instances.GetSGs() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestEC2Instances_DescribeVpcs(t *testing.T) {
 	type fields struct {
 		Client Ec2Client
